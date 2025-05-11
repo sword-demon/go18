06955f69e682f96d52a8d69033327267635f1233
王鑫123wangxin <1418667580@qq.com>
2025-05-11 11:49:34 +0800

    feat: 对读取配置函数进行单元测试

--- book/v2/config/config_test.go
+++ book/v2/config/config_test.go
@@ -1,1 +1,13 @@
 package config
+
+import (
+	"testing"
+)
+
+func TestLoadConfigFromYaml(t *testing.T) {
+	err := LoadConfigFromYaml("../application.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(C())
+}
