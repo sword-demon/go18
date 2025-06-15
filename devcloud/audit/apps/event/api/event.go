2e954297bc6df19fa7b32b2c0d04761958125a8f
王鑫123wangxin <1418667580@qq.com>
2025-06-15 19:47:36 +0800

    wx, feat: add pagination support to QueryEventApi

--- devcloud/audit/apps/event/api/event.go
+++ devcloud/audit/apps/event/api/event.go
@@ -4,10 +4,27 @@ import (
 	"github.com/emicklei/go-restful/v3"
 	"github.com/infraboard/mcube/v2/http/restful/response"
 	"github.com/sword-demon/go18/devcloud/audit/apps/event"
+	"strconv"
 )
 
 func (h *EventRestfulApiHandler) QueryEventApi(r *restful.Request, w *restful.Response) {
 	req := event.NewQueryEventRequest()
+
+	pageSizeStr := r.QueryParameter("page_size")
+	pageSize, err := strconv.ParseUint(pageSizeStr, 10, 64)
+	if err != nil {
+		response.Failed(w, err)
+		return
+	}
+	req.PageSize = pageSize
+
+	pageNumberStr := r.QueryParameter("page_number")
+	pageNumber, err := strconv.ParseUint(pageNumberStr, 10, 64)
+	if err != nil {
+		response.Failed(w, err)
+		return
+	}
+	req.PageNumber = pageNumber
 
 	// 执行逻辑
 	userM, err := h.event.QueryEvent(r.Request.Context(), req)
